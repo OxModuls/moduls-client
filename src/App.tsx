883c73e138d91256f31330c934beecfc30d9c47c
Moduls.Fun <modulsdotfun@gmail.com>
2025-07-23 16:11:50 +0100

    setup wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,26 @@ import RootLayout from "./components/root-layout";
 import Home from "./pages/home";
 import Token from "./pages/token";
 import CreateAgent from "./pages/create-agent";
+import { WagmiProvider } from "wagmi";
+import { config } from "./wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark">
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/agent" element={<Token />} />
-          <Route path="/create" element={<CreateAgent />} />
-        </Route>
-      </Routes>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/agent" element={<Token />} />
+              <Route path="/create" element={<CreateAgent />} />
+            </Route>
+          </Routes>
+        </QueryClientProvider>
+      </WagmiProvider>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import { Buffer } from "buffer";
+
+// Polyfill self for browser and global for Node.js
+const globalObject = typeof self !== "undefined" ? self : global;
+
+Object.assign(globalObject, {
+  Buffer: Buffer,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
